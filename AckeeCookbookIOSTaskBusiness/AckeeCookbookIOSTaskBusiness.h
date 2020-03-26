d0c6cda26598b6f7515393d91123df8853d2fd3a
Ihor Myroniuk <ihormyroniuk@gmail.com>
2020-03-26 18:55:15 +0200

    Create "Business" module

---
+++ AckeeCookbookIOSTaskBusiness/AckeeCookbookIOSTaskBusiness.h
@@ -0,0 +1,19 @@
+//
+//  AckeeCookbookIOSTaskBusiness.h
+//  AckeeCookbookIOSTaskBusiness
+//
+//  Created by Ihor Myroniuk on 3/26/20.
+//  Copyright © 2020 Ihor Myroniuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AckeeCookbookIOSTaskBusiness.
+FOUNDATION_EXPORT double AckeeCookbookIOSTaskBusinessVersionNumber;
+
+//! Project version string for AckeeCookbookIOSTaskBusiness.
+FOUNDATION_EXPORT const unsigned char AckeeCookbookIOSTaskBusinessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AckeeCookbookIOSTaskBusiness/PublicHeader.h>
+
+
